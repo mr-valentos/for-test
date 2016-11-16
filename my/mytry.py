51a0fb0a33d2633671f58dddb92d121ac8118efe
mr-valentos <nerpavlad@gmail.com>
2016-11-16 16:03:03 +0200

    homework1

--- my/mytry.py
+++ my/mytry.py
@@ -11,12 +11,14 @@ previous_job = input("Where did you work? ")
 experience = input("What is your work experience? ")
 name_surname = name + ' ' + surname
 
+list_bye = ['Goodbye', name, surname]
 
 info = "Thank you %s, your age is %i. \n We will try to find work in the city %s." \
        " \n Job vacancy - %s \n Experience - %s" % (name_surname, age, city, previous_job, experience)
 
 print(info)
 print("We will send you letter, check your postbox.")
+print (list_bye)
 
 file_info.write(info)
 file_info.close()
